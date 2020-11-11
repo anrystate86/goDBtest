08a5fc65bc53298ff28d8b851dd2be896f28b404
Andrey Nazarov <35319025+anrystate86@users.noreply.github.com>
2020-11-11 11:37:27 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 
 	_ "github.com/godror/godror"
 	_ "github.com/lib/pq"
-	//_ "gopkg.in/goracle.v2"
-	//	"log"
 )
 
 func testConnstrinng(tconnstr string) (string, string, string) {
@@ -47,46 +45,25 @@ func checkDB(ctypeDB, cconStr, cuser, cpass, cdb string) string {
 		return "error"
 	}
 
-	//return cquerry, cconnString
-
-	//jdbc:oracle:thin:@eb-exp-demo-poi-db.otr.ru:1531/ebpoi
-	//jdbc:postgresql://sp-test-poi-db.otr.ru/spdev60?:5432/ufosq
-
-	//connStr := `user="puser" password="123456" connectString="localhost:1521/orclpdb1"`
-	// fmt.Sprintf("host=%s port=%d dbname=%s user=%s password='%s' sslmode=disable search_path=%s", ...)
-	//db, err := sql.Open("godror", connStr)
-
-	//connStr := "user=puser password=123456 dbname=postgres sslmode=disable"
-	//db, err := sql.Open("postgres", connStr)
-
 	db, err := sql.Open(ctypeDB, cconnString)
 	if err != nil {
-		//panic(err)
-		//log.Fatal("Failed connect to DB : ", err)
 		fmt.Println(err)
-		return "" //, err.Error()
+		return ""
 	}
 	defer db.Close()
 
 	var test responce
-	//userSql := "SELECT 1 from dual"
-	//userSql := "SELECT 1 as test FROM pg_database WHERE datname='postgres'"
-
 	err = db.QueryRow(cquerry).Scan(&test.tresp)
 	if err != nil {
-		//log.Fatal("Failed to execute query: ", err)
 		fmt.Println(err)
-		return "" //, err.Error()
+		return ""
 	}
 
-	//fmt.Printf("%s\n", test.tresp)
 	if test.tresp == "1" {
 		return "OK"
 	}
 	fmt.Println(err)
 	return ""
-
-	//return "", test.tresp
 }
 
 func main() {
@@ -98,26 +75,14 @@ func main() {
 	flag.Parse()
 	// Проверка добавления параметров запуска утилиты
 	if (*userPtr != "usernamevar") && (*passPtr != "passwordvar") && (*constrPtr != "connection string") {
-		//fmt.Println("user:", *userPtr)
-		//fmt.Println("password:", *passPtr)
-		//fmt.Println("connection string:", *constrPtr)
-		//-----------------------------------------------------------
-
 		// Проверка строки подключения
-		//connstr := "jdbc:oracle:thin:@eb-exp-demo-poi-db.otr.ru:1531/ebpoi"
-		//connstr := "jdbc:postgresql://sp-test-poi-db.otr.ru/spdev60?:5432/ufos"
-		//testConnstrinng(connstr)
 		typeDB, connectionString, dbName := testConnstrinng(*constrPtr)
 		if typeDB != "error" {
 			fmt.Println(checkDB(typeDB, connectionString, *userPtr, *passPtr, dbName))
 		} else {
 			fmt.Println(connectionString)
 		}
-		//fmt.Println(testConnstrinng(*constrPtr))
 	} else {
-		//fmt.Println("user:", *userPtr)
-		//fmt.Println("password:", *passPtr)
-		//fmt.Println("connection string:", *constrPtr)
 		fmt.Println("wrong usage, must testConnDB -user USERNAME -pass PASSWORD -constr CONNECTION_STRING_FOR_DB")
 	}
 
